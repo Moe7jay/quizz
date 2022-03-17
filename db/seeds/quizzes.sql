92584816f6de95f6d3a0cd9be23aeab7e5d299f5
Moe7jay <101473903+Moe7jay@users.noreply.github.com>
2022-03-16 20:12:00 -0400

    quiz

---
+++ db/seeds/quizzes.sql
@@ -0,0 +1,4 @@
+INSERT INTO quizzes (user_id, name, description, isPrivate) VALUES(1, 'Mathematics', 'Basic Addition', FALSE);
+INSERT INTO quizzes (user_id, name, description, isPrivate) VALUES(2, 'Mathematics', 'Basic Multiplication', FALSE);
+INSERT INTO quizzes (user_id, name, description, isPrivate) VALUES(1, 'Mathematics', 'Basic Subtraction', FALSE);
+INSERT INTO quizzes (user_id, name, description, isPrivate) VALUES(1, 'Mathematics', 'Basic Division', FALSE);
